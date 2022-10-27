d0c3e42429674583eb5b7f2702ed0180436adb5e
SamJay321 <itzsamjay321@gmail.com>
2022-10-27 23:23:11 +0100

    switch

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHtml;
 
+    switch (buttonInnerHtml) {
+      case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
 
+      case "a":
+        var kick = new Audio("sounds/kick.mp3");
+        kick.play();
+        break;
 
+      case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      default:
+        break;
+    }
   });
 }
-
-// var audio = new Audio("sounds/tom-3.mp3");
-// audio.play();
